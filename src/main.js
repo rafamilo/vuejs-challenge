012e0e0dd1ab1ad9ce29ce8b0f2dc0b43b45d177
reginaldo <camilo.melges@gmail.com>
2020-03-24 00:06:51 -0400

    Added vue-router

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from "vue";
 import App from "./App.vue";
-import 'es6-promise/auto'
+import "es6-promise/auto";
 import Vuex from "vuex";
+import VueRouter from "vue-router";
 
 Vue.config.productionTip = false;
 Vue.use(Vuex);
+Vue.use(VueRouter);
 
 new Vue({
   render: h => h(App)
