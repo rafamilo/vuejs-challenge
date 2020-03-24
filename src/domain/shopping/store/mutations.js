0addab3b6569e2b2ee0a2239b4c35c3b08cb500c
reginaldo <camilo.melges@gmail.com>
2020-03-23 23:18:40 -0400

    Added mutations to shoppings

--- src/domain/shopping/store/mutations.js
+++ src/domain/shopping/store/mutations.js
@@ -1,3 +1,5 @@
 export default {
-  
+  SET_SHOPPINGS: (state, shoppings) => state.shoppings = shoppings,
+  PUSH_SHOPPINGS: (state, shopping) => state.shoppings.push(shopping),
+  SET_LOADING_LIST: (state, loading) => state.loading.list = loading,
 }
