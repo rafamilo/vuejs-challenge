e9eb6deabdced8fc4553ef7797885114dceef68b
reginaldo <camilo.melges@gmail.com>
2020-03-26 12:41:05 -0400

    added order and filter by text

--- src/domain/shopping/components/column.vue
+++ src/domain/shopping/components/column.vue
@@ -1,27 +1,29 @@
 <template>
   <div class="column">
     <b-button class="remove-column" pill size="sm" variant="success" @click="removeColumn()">
-        <i class="fas fa-times" />
-      </b-button>
-    <b-input-group slot="header">
-      <template v-slot:append>
-        <b-input-group-text>Go!</b-input-group-text>
-      </template>
-      <b-form-input></b-form-input>
+      <i class="fas fa-times" />
+    </b-button>
+    <b-input-group slot="header" append="Go!">
+      <b-form-input type="text" placeholder="Text" v-model="textFilter"></b-form-input>
     </b-input-group>
+
     <div class="order d-flex align-items-center justify-content-between pt-2">
       <b-button pill size="sm" variant="success" @click="addShopping()">
         <i class="fas fa-plus" />
       </b-button>
       <div class="text d-flex align-items-center justify-content-end">
         <span>Order</span>
-        <i class="fas fa-sort-up" />
-        <i class="fas fa-sort-down" />
+        <span @click="orderAscShoppings(true)">
+          <i class="fas fa-sort-up" />
+        </span>
+        <span @click="orderAscShoppings(false)">
+          <i class="fas fa-sort-down" @click="orderAscShoppings(false)" />
+        </span>
       </div>
     </div>
     <draggable v-model="column.shoppings" group="shoppings" @start="drag=true" @end="drag=false">
       <shopping
-        v-for="(shopping, index) in column.shoppings"
+        v-for="(shopping, index) in filteredShoppings"
         :key="index"
         :shoppingIndex="index"
         :columnIndex="columnIndex"
@@ -49,6 +51,17 @@ export default {
     column: { type: Object, default: () => {} },
     columnIndex: { type: Number, required: true }
   },
+  data() {
+    return {
+      textFilter: "",
+      filteredShoppings: this.column.shoppings
+    };
+  },
+  watch: {
+    textFilter: function(text) {
+      this.filterShoppingsByText(text);
+    }
+  },
   methods: {
     ...mapMutations(["PUSH_SHOPPING", "REMOVE_COLUMN"]),
     addShopping() {
@@ -60,6 +73,17 @@ export default {
     },
     removeColumn() {
       this.REMOVE_COLUMN(this.columnIndex);
+    },
+    filterShoppingsByText(text) {
+      this.filteredShoppings = this.column.shoppings.filter(shopping => {
+        return shopping.text.includes(text);
+      });
+    },
+    orderAscShoppings(asc) {
+      if (asc) {
+        return this.filteredShoppings.sort((a, b) => (a.id > b.id ? 1 : -1));
+      }
+      return this.filteredShoppings.sort((a, b) => (a.id > b.id ? -1 : 1));
     }
   }
 };
@@ -99,6 +123,9 @@ export default {
         display: block;
         margin-right: 10px;
         margin-bottom: -5px;
+      }
+      .fa-sort-down, .fa-sort-up {
+        cursor: pointer;
       }
       .fa-sort-up {
         margin-bottom: -20px;
